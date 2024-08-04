ccebe4a57257163d2b52fa3e04e471e1c8738f3b
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-04 15:45:30 -0300

    Ex005

---
+++ leetcode/src/leetcode/exercise/Ex005_LongestCommomPrefix/Ex005.java
@@ -0,0 +1,30 @@
+package leetcode.exercise.Ex005_LongestCommomPrefix;
+
+import java.util.Arrays;
+
+public class Ex005 {
+
+	public static void main(String[] args) {
+		String[] palavras = new String[] {"flor","fluxo","voo"};
+		String prefixo = longestCommonPrefix(palavras);
+		System.out.println(prefixo.toString());
+	}
+
+	public static String longestCommonPrefix(String[] strs) {
+		StringBuilder resposta = new StringBuilder();
+		Arrays.sort(strs);
+		String primeira = strs[0];
+		String ultima = strs[strs.length - 1];
+		int tam = Integer.min(primeira.length(), ultima.length());
+		
+		for (int i = 0; i < tam; i++) {
+			if (primeira.charAt(i) != ultima.charAt(i)) {
+				return resposta.toString();
+			} else {
+				resposta.append(primeira.charAt(i));
+			}
+		}
+		
+		return resposta.toString();
+	}
+}
